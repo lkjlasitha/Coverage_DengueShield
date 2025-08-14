f006d0911030f3f60adf4ce61eaba370045d882a
Lasitha Lakshan <134188067+lkjlasitha@users.noreply.github.com>
2025-08-14 17:57:20 +0530

    profile page

--- dengue_shield/lib/screens/main/action_page.dart
+++ dengue_shield/lib/screens/main/action_page.dart
@@ -1,5 +1,6 @@
 import 'package:dengue_shield/widgets/appbar/appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:dotted_border/dotted_border.dart';
 import '../../widgets/button/button.dart';
 
 class ActionScreen extends StatefulWidget {
@@ -10,42 +11,79 @@ class ActionScreen extends StatefulWidget {
 }
 
 class _ActionScreenState extends State<ActionScreen> {
+  bool showBreedingSitesContainer = false;
+  bool showMosquitoReportsContainer = false;
 
-  Widget _container(Color containerColor , Color textColor , String content) {
+  Widget _container(Color containerColor, Color textColor, String content,
+      {required VoidCallback onTap, required bool showArrowUp}) {
     final double screenWidth = MediaQuery.of(context).size.width;
-    return Container(
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: containerColor,
-        border: Border.all(
-          width: 0.5,
-          color: Color(0xff000000).withOpacity(0.13)
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: containerColor,
+          border: Border.all(
+            width: 0.5,
+            color: Color(0xff000000).withOpacity(0.13),
+          ),
+          borderRadius: BorderRadius.circular(10),
         ),
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10 , vertical: 15),
-        child: Stack(
-          children: [
-            Center(
-              child: Text(
-                content,
-                style: TextStyle(
-                  fontSize: screenWidth*0.04,
-                  color: textColor,
-                  fontWeight: FontWeight.w600,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+          child: Stack(
+            children: [
+              Center(
+                child: Text(
+                  content,
+                  style: TextStyle(
+                    fontSize: screenWidth * 0.04,
+                    color: textColor,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
               ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Icon(
+                  showArrowUp
+                      ? Icons.keyboard_arrow_up_outlined
+                      : Icons.keyboard_arrow_down_outlined,
+                  color: textColor,
+                  size: screenWidth * 0.07,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _dottedContainer(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return Padding(
+      padding: const EdgeInsets.only(top: 18.0, bottom: 12.0),
+      child: DottedBorder(
+        color: Color(0xffC1C0C5),
+        strokeWidth: 1.6,
+        borderType: BorderType.RRect,
+        radius: Radius.circular(14),
+        dashPattern: [6, 6],
+        child: Container(
+          height: screenWidth * 0.26,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(14),
+          ),
+          child: Center(
+            child: Icon(
+              Icons.add,
+              color: Color(0xffC1C0C5),
+              size: screenWidth * 0.09,
             ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: Icon(
-                Icons.keyboard_arrow_down_outlined,
-                color: textColor,
-                size: screenWidth*0.07,
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
@@ -59,67 +97,82 @@ class _ActionScreenState extends State<ActionScreen> {
         children: [
           Appbar(),
           Positioned(
-            top: screenWidth * 0.375,
+            top: screenWidth * 0.35,
             left: screenWidth * 0.04,
             right: screenWidth * 0.04,
             child: SizedBox(
-              height: MediaQuery.of(context).size.height - (screenWidth * 0.375),
+              height: MediaQuery.of(context).size.height - (screenWidth * 0.35),
               child: SingleChildScrollView(
                 child: Column(
                   children: [
                     Text(
                       "What would you like to report?",
                       style: TextStyle(
-                        fontSize: screenWidth*0.055,
+                        fontSize: screenWidth * 0.055,
                         fontWeight: FontWeight.w700,
-                        color: Colors.white
+                        color: Colors.white,
                       ),
                     ),
-                    SizedBox(
-                      height: screenWidth*0.1,
-                    ),
+                    SizedBox(height: screenWidth * 0.1),
                     Text(
                       "Help us fight mosquito-borne diseases by reporting \nbreeding sites or mosquito activity in your area. \nSelect the type of case below and submit \nyour report to alert our control teams.",
                       textAlign: TextAlign.center,
                       style: TextStyle(
-                        fontSize: screenWidth*0.0325,
+                        fontSize: screenWidth * 0.0325,
                         fontWeight: FontWeight.w500,
-                        color: Color(0xff6B7280)
+                        color: Color(0xff6B7280),
                       ),
                     ),
-                    SizedBox(
-                      height: screenWidth*0.075,
+                    SizedBox(height: screenWidth * 0.065),
+                    _container(
+                      Color(0xffEB4335).withOpacity(0.21),
+                      Color(0xffEB4335),
+                      "Breeding Sites",
+                      onTap: () {
+                        setState(() {
+                          showBreedingSitesContainer = !showBreedingSitesContainer;
+                        });
+                      },
+                      showArrowUp: showBreedingSitesContainer,
                     ),
-                    _container(Color(0xffEB4335).withOpacity(0.21), Color(0xffEB4335), "Breeding Sites"),
-                    SizedBox(
-                      height: screenWidth*0.05,
+                    if (showBreedingSitesContainer) _dottedContainer(context),
+                    SizedBox(height: screenWidth * 0.04),
+                    _container(
+                      Color(0xffDDDDDD),
+                      Color(0xff6B7280),
+                      "Mosquito Activity Reports",
+                      onTap: () {
+                        setState(() {
+                          showMosquitoReportsContainer = !showMosquitoReportsContainer;
+                        });
+                      },
+                      showArrowUp: showMosquitoReportsContainer,
                     ),
-                    _container(Color(0xffDDDDDD), Color(0xff6B7280), "Mosquito Activity Reports"),
-                    SizedBox(
-                      height: screenWidth*0.065,
-                    ),
-                    SharedButton(screenWidth: screenWidth, 
-                    content: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Text(
+                    if (showMosquitoReportsContainer) _dottedContainer(context),
+                    SizedBox(height: screenWidth * 0.08),
+                    SharedButton(
+                      screenWidth: screenWidth,
+                      content: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Text(
                             'Suspected Cases',
                             style: TextStyle(
-                              fontSize: screenWidth*0.0425,
+                              fontSize: screenWidth * 0.0425,
                               fontWeight: FontWeight.w600,
-                              color: Colors.white
+                              color: Colors.white,
                             ),
                           ),
                         ),
-                      )
-                    )
+                      ),
+                    ),
                   ],
                 ),
               ),
-            )
+            ),
           ),
         ],
-      )
+      ),
     );
   }
 }
--- dengue_shield/lib/widgets/bottom_navigation_bar/bottom_navigation_bar.dart
+++ dengue_shield/lib/widgets/bottom_navigation_bar/bottom_navigation_bar.dart
@@ -8,8 +8,9 @@ import 'package:dengue_shield/screens/main/profile_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:animations/animations.dart';
 import '../alret_dialog/alert_dialog.dart';
-import '../appbar/appbar.dart';
+
 
 class CustomBottomNav extends StatefulWidget {
   const CustomBottomNav({Key? key}) : super(key: key);
@@ -20,80 +21,65 @@ class CustomBottomNav extends StatefulWidget {
 
 class CustomBottomNavState extends State<CustomBottomNav> {
   int _selectedIndex = 0;
-  final PageController _pageController = PageController();
   bool _isLoading = false;
-
 
   final List<_NavItem> _navItems = [
     _NavItem(label: 'Home', iconPath: 'assets/navigation_bar/home.svg'),
-    _NavItem(label: 'inform', iconPath: 'assets/navigation_bar/inform.svg'),
-    _NavItem(label: 'map', iconPath: 'assets/navigation_bar/map.svg'),
-    _NavItem(label: 'action', iconPath: 'assets/navigation_bar/action.svg'),
+    _NavItem(label: 'Inform', iconPath: 'assets/navigation_bar/inform.svg'),
+    _NavItem(label: 'Map', iconPath: 'assets/navigation_bar/map.svg'),
+    _NavItem(label: 'Action', iconPath: 'assets/navigation_bar/action.svg'),
     _NavItem(label: 'Profile', iconPath: 'assets/navigation_bar/profile.svg'),
   ];
 
-  void navigateTo(int index) {
-    if (_selectedIndex != index) {
-      setState(() {
-        _selectedIndex = index;
-      });
-      _pageController.animateToPage(
-        index,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeOut,
-      );
-    }
-  }
+  // Your pages
+  final List<Widget> _pages = const [
+    HomeScreen(),
+    InformScreen(),
+    MapScreen(),
+    ActionScreen(),
+    ProfileScreen()
+  ];
 
   void _onItemTapped(int index) {
-    setState(() => _selectedIndex = index);
-    _pageController.animateToPage(
-      index,
-      duration: const Duration(milliseconds: 300),
-      curve: Curves.easeOut,
-    );
+    if (_selectedIndex != index) {
+      setState(() => _selectedIndex = index);
+    }
   }
 
   Widget _buildNavItem(int index) {
     final isSelected = index == _selectedIndex;
     final screenWidth = MediaQuery.of(context).size.width;
+
     return Expanded(
       child: GestureDetector(
-        onTap: () => {
-          _onItemTapped(index)
-        },
-        child: AnimatedContainer(
-          duration: const Duration(milliseconds: 300),
-          curve: Curves.easeOut,
-          //padding: const EdgeInsets.symmetric(vertical: 0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox(height: screenWidth*0.03),
-              AnimatedScale(
-                scale: isSelected ? 1.0 : 0.8,
-                duration: const Duration(milliseconds: 300),
-                child: SvgPicture.asset(
-                  _navItems[index].iconPath,
-                  height: 24,
-                  colorFilter: ColorFilter.mode(
-                    isSelected ? mainColor : Colors.black,
-                    BlendMode.srcIn,
-                  ),
+        onTap: () => _onItemTapped(index),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(height: screenWidth * 0.03),
+            AnimatedScale(
+              scale: isSelected ? 1.0 : 0.8,
+              duration: const Duration(milliseconds: 300),
+              child: SvgPicture.asset(
+                _navItems[index].iconPath,
+                height: 24,
+                colorFilter: ColorFilter.mode(
+                  isSelected ? mainColor : Colors.black,
+                  BlendMode.srcIn,
                 ),
               ),
-              SizedBox(height: screenWidth*0.01),
-              Text(
-                _navItems[index].label,
-                style: TextStyle(
-                  fontSize: screenWidth*0.03,
-                  color: isSelected ? mainColor : Colors.black,
-                  fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
-                ),
+            ),
+            SizedBox(height: screenWidth * 0.01),
+            Text(
+              _navItems[index].label,
+              style: TextStyle(
+                fontSize: screenWidth * 0.03,
+                color: isSelected ? mainColor : Colors.black,
+                fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
               ),
-              SizedBox(height: screenWidth*0.025),
-            ],
-          ),
+            ),
+            SizedBox(height: screenWidth * 0.025),
+          ],
         ),
       ),
     );
@@ -108,57 +94,49 @@ class CustomBottomNavState extends State<CustomBottomNav> {
         confirmText: 'OK',
         cancelText: 'Cancel',
         onConfirmed: () {
-          Navigator.of(context).pop(true); 
+          Navigator.of(context).pop(true);
         },
       ),
     );
 
     if (result == true) {
       if (Platform.isAndroid) {
-        SystemNavigator.pop(); 
+        SystemNavigator.pop();
       } else if (Platform.isIOS) {
-        exit(0); 
+        exit(0);
       }
       return true;
     }
-
-    return false; 
+    return false;
   }
-  
+
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
+
     if (_isLoading) {
       return const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
+        body: Center(child: CircularProgressIndicator()),
       );
     }
+
     return WillPopScope(
       onWillPop: () => _onWillPop(context),
       child: Scaffold(
-        body: Column(
-          children: [
-            Expanded(
-              child: PageView(
-                controller: _pageController,
-                physics: const NeverScrollableScrollPhysics(),
-                children: [
-                  HomeScreen(),
-                  InformScreen(),
-                  MapScreen(),
-                  ActionScreen(),
-                  ProfileScreen()
-                ],
-              ),
-            ),
-          ],
+        body: PageTransitionSwitcher(
+          duration: const Duration(milliseconds: 400),
+          transitionBuilder: (child, animation, secondaryAnimation) => FadeThroughTransition(
+            animation: animation,
+            secondaryAnimation: secondaryAnimation,
+            child: child,
+          ),
+          child: _pages[_selectedIndex],
         ),
         bottomNavigationBar: Container(
           decoration: BoxDecoration(
             color: Colors.white,
-            borderRadius:  BorderRadius.vertical(top: Radius.circular(screenWidth*0.04)),
+            borderRadius:
+                BorderRadius.vertical(top: Radius.circular(screenWidth * 0.04)),
             boxShadow: [
               BoxShadow(
                 color: Colors.black.withOpacity(0.05),
@@ -166,9 +144,11 @@ class CustomBottomNavState extends State<CustomBottomNav> {
               ),
             ],
           ),
-          //padding: const EdgeInsets.symmetric(vertical: 10),
           child: Row(
-            children: List.generate(_navItems.length, (index) => _buildNavItem(index)),
+            children: List.generate(
+              _navItems.length,
+              (index) => _buildNavItem(index),
+            ),
           ),
         ),
       ),
@@ -179,6 +159,5 @@ class CustomBottomNavState extends State<CustomBottomNav> {
 class _NavItem {
   final String label;
   final String iconPath;
-
   _NavItem({required this.label, required this.iconPath});
 }
